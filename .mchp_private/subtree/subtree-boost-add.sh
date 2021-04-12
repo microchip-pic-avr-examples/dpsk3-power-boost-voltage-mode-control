0511e9cd9cbaa6eb33771207fdecc247e5a66902
Microchip Technology <microchip@microchip.com>
2021-04-12 10:33:27 +0000

    1.2.1

--- .mchp_private/subtree/subtree-boost-add.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo $SSH_AUTH_SOCK
-cd ../../
-git remote add subrepo-boost https://bitbucket.microchip.com/scm/mcu16asmpsl/subrepo-boost-converter.git
-git subtree add --prefix="dpsk_boost_vmc.X/sources/power_control/devices" subrepo-boost main --squash
-echo Press Enter to exit
-read
