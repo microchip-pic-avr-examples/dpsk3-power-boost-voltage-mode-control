032bf27b2bc63d34af7111a3ef38ad42eb9f7ce3
Microchip Technology <microchip@microchip.com>
2021-08-27 17:12:52 +0000

    1.2.11

--- .mchp_private/subtree/subtree-boost-push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo $SSH_AUTH_SOCK
-cd ../../
-git subtree push --prefix="dpsk_boost_vmc.X/sources/power_control/devices" subrepo-boost feature/version-update --squash
-echo Press Enter to exit
-read
-
